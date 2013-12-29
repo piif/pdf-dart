023458da9942c1d33991291c594e57fc982c14c3
piif <christian_lefebvre@laposte.net>
2013-12-29 21:33:22 +0100

    - "delete" method renamed "abort" - added "measure unit" system - some inline doc - example shows units and more "arc" usage

--- example/helloPdf.dart
+++ example/helloPdf.dart
@@ -9,11 +9,19 @@ void main() {
 		pdf.setOption("stringformat", "utf8");
 	}
 
-	pdf.setInfo("Creator", "helloPdf.dart");
-	pdf.setInfo("Author", "Christian Lefebvre");
-	pdf.setInfo("Title", "Hello, Dart world !");
+	pdf..setInfo("Creator", "helloPdf.dart")
+		..setInfo("Author", "Christian Lefebvre")
+		..setInfo("Title", "Hello, Dart world !");
 
 	pdf.beginPage(PDF.pageSize["a4"]); // ~ 600x800
+
+	var image1 = pdf.loadImage("chabenet.jpg");
+	var image2 = pdf.loadImage("chabenet.png");
+
+	pdf.imageTo(image1, 50, 650,
+		"boxsize={200 150} position={center} fitmethod=meet showborder");
+	pdf.imageTo(image2, 350, 650,
+		"scale=0.22 boxsize={150 100} position={right top} fitmethod=clip showborder");
 
 	var font = pdf.loadFont("Helvetica-Bold");
 
@@ -35,56 +43,54 @@ void main() {
 	etoile(pdf, 300, 400, PDF.STROKE | PDF.CLOSE);
 	etoile(pdf, 500, 400, PDF.FILL_STROKE);
 
-	pdf..circle(100, 200, 50)
+	pdf.setUnit(PDF.unitName["millimeter"]);
+	pdf.setLineWidth(10.0);
+
+	pdf..circle(35, 70, 20)
 		..terminatePath(PDF.STROKE);
-	pdf..circle(100, 200, 30)
+	pdf..circle(35, 70, 10)
+		..terminatePath(PDF.FILL);
+	pdf.setLineWidth(1.0);
+
+	pdf..arc(105, 70, 25, 30, 180)
+		..terminatePath(PDF.FILL_STROKE);
+	pdf..arc(105, 70, 25, 0, 210, true)
+		..terminatePath(PDF.STROKE | PDF.CLOSE);
+	pdf..arc(105, 70, 27, 5, 25)
+		..arc(105, 70, 27, 205, 185, true)
+		..terminatePath(PDF.STROKE | PDF.CLOSE);
+	pdf..arc(105, 70, 20, -110, -40)
 		..terminatePath(PDF.FILL);
 
-	pdf..arc(300, 200, 50, 30, 180)
-		..terminatePath(PDF.FILL_STROKE);
-	pdf..arc(300, 200, 50, 210, 0)
-		..terminatePath(PDF.STROKE | PDF.CLOSE);
+	pdf..save()
+		..translate(175, 70);
 
-	num yy = sin(PI/3);
+	rosace(pdf, 20, PDF.STROKE);
+	pdf.rotate(60);
+	rosace(pdf, 20, PDF.STROKE);
 
-	rosace(pdf, 500, 200, 50, [
-		[ 0, -1, 30, 150 ],
-		[ -yy, 0.5, -90, 30 ],
-		[ yy, 0.5, 150, -90 ]
-	], PDF.STROKE);
-	rosace(pdf, 500, 200, 50, [
-		[ 0, 1, -150, -30 ],
-		[ yy, -0.5, 90, 210 ],
-		[ -yy, -0.5, -30, 90 ]
-	], PDF.STROKE);
+	pdf.rotate(30);
 
-	rosace(pdf, 500, 200, 50, [
-		[ -1, 0, -60, 60 ],
-		[ 0.5, yy, 180, -60 ],
-		[ 0.5, -yy, 60, 180 ]
-	], PDF.FILL_STROKE);
-	rosace(pdf, 500, 200, 50, [
-		[ 1, 0, 120, 240 ],
-		[ -0.5, -yy, 0, 120 ],
-		[ -0.5, yy, -120, 0 ]
-	], PDF.FILL_STROKE);
+	rosace(pdf, 25, PDF.FILL_STROKE);
+	pdf.rotate(60);
+	rosace(pdf, 25, PDF.FILL_STROKE);
 
-	var image1 = pdf.loadImage("chabenet.jpg");
-	var image2 = pdf.loadImage("chabenet.png");
-
-	pdf.imageTo(image1, 50, 650,
-		"boxsize={200 150} position={center} fitmethod=meet showborder");
-	pdf.imageTo(image2, 350, 650,
-		"scale=0.22 boxsize={150 100} position={right top} fitmethod=clip showborder");
+	pdf..restore();
 
 	pdf.endPage();
 
 	pdf.close();
 }
 
-void rosace(PDF pdf, num x, num y, num r, List<List<num>> specs, what) {
+void rosace(PDF pdf, num r, what) {
+	num yy = sin(PI/3);
+	List<List<num>> specs = [
+		[ -1, 0, -60, 60 ],
+		[ 0.5, yy, 180, -60 ],
+		[ 0.5, -yy, 60, 180 ]
+	];
 	specs.forEach((List<num> spec) {
-		pdf.arc(x + spec[0]*r, y + spec[1]*r, r, spec[2], spec[3]);
+		pdf.arc(spec[0]*r, spec[1]*r, r, spec[2], spec[3]);
 	});
 	pdf.terminatePath(what);
 }
--- lib/pdfdart.dart
+++ lib/pdfdart.dart
@@ -8,6 +8,19 @@ class PDF {
 	int handle;
 	Future f;
 
+	/**
+	 * default pdf measure unit is "point" (1/72 inch). This table contains
+	 * some ratio to use other units to pass to {@see setUnit} method
+	 */
+	static Map<String,double> unitName = {
+		"point": 1,
+		"millimeter": 0.3528,
+		"inch": 0.0139
+	};
+
+	/**
+	 * some standard paper sizes to pass to {@see beginPage} method
+	 */
 	static Map<String,List<double>> pageSize = {
 		"a0" : [ 2380.0, 3368.0 ],
 		"a1" : [1684.0, 2380.0 ],
@@ -23,10 +36,15 @@ class PDF {
 		"11x17"  : [792.0, 1224.0 ]
 	};
 
+	/**
+	 * constants to pass to {@see terminatePath} method
+	 */
 	static final int STROKE = 1;
 	static final int FILL = 2;
 	static final int FILL_STROKE = 3;
 	static final int CLOSE = 4;
+
+	double _unit = unitName["point"];
 
 	/**
 	 * prepare new PDF document
@@ -35,9 +53,7 @@ class PDF {
 		ReceivePort response = new ReceivePort();
 
 		_servicePort.send([response.sendPort, "create", filename, options]);
-//		print("create ...");
 		f = response.first.then((List result) {
-//			print("create");
 			if (result[0] == null) {
 				handle = result[1];
 			} else {
@@ -49,12 +65,31 @@ class PDF {
 	}
 
 	/**
-	 * abort document
+	 * many PDF methods are asynchonous. This method runs it's callback when
+	 * all pending actions are achieved.
 	 */
-	void delete() {
+	Future then(onValue, {onError: null}) {
+		return f.then(onValue, onError: onError);
+	}
+
+	/**
+	 * Change current measure unit. All method calls made after will divide
+	 * lenght arguments by this value before passing them to pdflib
+	 * EXCEPTED for pageSize argument in {@see beginPath}
+	 */
+	void setUnit(num ratio) {
+		f = f.then((_) {
+			_unit = ratio;
+		});
+	}
+
+	/**
+	 * abort document. PDF object must not be used after.
+	 */
+	void abort() {
 		f = f.then((_) {
 			ReceivePort response = new ReceivePort();
-			_servicePort.send([response.sendPort, "delete", handle]);
+			_servicePort.send([response.sendPort, "abort", handle]);
 			return response.first;
 		}).then((List result) {
 			if (result[0] != null) {
@@ -62,12 +97,12 @@ class PDF {
 			}
 			f = null;
 		}).catchError((e){
-			print("exception on delete : ${e}");
+			print("exception on abort : ${e}");
 		});
 	}
 
 	/**
-	 * save document and free structure
+	 * save document and free structure. PDF object must not be used after.
 	 */
 	void close() {
 		f = f.then((_) {
@@ -88,11 +123,18 @@ class PDF {
 		_doCall("setInfo", [info, value]);
 	}
 
+	/**
+	 * starts a new page, with given size and optional options
+	 */
 	void beginPage(List<double> size, [ String options = "" ]) {
 		_doCall("beginPage", [size[0], size[1], options]);
 	}
 
-	Future loadFont(String fontName, [ String options = "" ]) {
+	/**
+	 * Load and prepare a font
+	 * @return [Future<int>] whrn font is loaded, int is a handle to refer to in {@see setFont}
+	 */
+	Future<int> loadFont(String fontName, [ String options = "" ]) {
 		return f.then((_) {
 			ReceivePort response = new ReceivePort();
 			_servicePort.send([response.sendPort, "loadFont", handle, fontName, options]);
@@ -108,11 +150,12 @@ class PDF {
 		});
 	}
 
-	void setFont(Future<int> font, int size) {
+	void setFont(Future<int> font, num size) {
 		f = Future.wait([font, f]).then((List responses) {
 			int fontHandle = responses[0];
 			ReceivePort response = new ReceivePort();
-			_servicePort.send([response.sendPort, "setFont", handle, fontHandle, size]);
+			_servicePort.send([response.sendPort, "setFont", handle,
+				fontHandle, (size / _unit).round()]);
 			return response.first;
 		}).then((List result) {
 			if (result[0] != null) {
@@ -123,61 +166,102 @@ class PDF {
 		});
 	}
 
-	// if pos is not given, text may start with '\n' to specify to
-	// draw text under last line
+	/**
+	 * Draw text with current font and stroke style.
+	 * If pos is not given, text starts at current position, or
+	 * if text starts with '\n', under last text line
+	 */
 	void textTo(String text, [num x= null, num y= null, options= ""]) {
 		if ((text == null || x == null) && options != "") {
 			throw new Exception("options used only if text AND pos are specified");
 		}
-		_doCall("textTo", [text, x, y, options]);
+		_doCall("textTo", () {
+			return [text, (x != null) ? x / _unit : x, (y != null) ? y / _unit : y, options];
+		});
 	}
 
+	/**
+	 * terminates current page
+	 */
 	void endPage() {
 		_doCall("endPage");
 	}
 
+	/**
+	 * save current styles and position
+	 */
 	void save() {
 		_doCall("save");
 	}
 
+	/**
+	 * restore last saved styles and position
+	 */
 	void restore() {
 		_doCall("restore");
 	}
 
+	/**
+	 * Change current coordinate system
+	 */
 	void translate(num x, num y) {
-		_doCall("translate", [x, y]);
+		_doCall("translate", () { return [x / _unit, y / _unit]; });
 	}
 
+	/**
+	 * Change current coordinate system
+	 */
 	void rotate(num angle) {
 		_doCall("rotate", [angle]);
 	}
 
+	/**
+	 * Set current stroke width
+	 */
 	void setLineWidth(num width) {
-		_doCall("setLineWidth", [width]);
+		_doCall("setLineWidth", () { return [width / _unit]; });
 	}
 
+	/**
+	 * Set current stroke or fill color.
+	 * Color components must be values between 0 and 1
+	 *
+	 */
 	void setColor(String kind, num r, num g, num b) {
 		_doCall("setColor", [kind, r, g, b]);
 	}
 
+	/**
+	 * Set current position
+	 */
 	void moveTo(num x, num y) {
-		_doCall("moveTo", [x, y]);
+		_doCall("moveTo", () { return [x / _unit, y / _unit]; });
 	}
 
 	void lineTo(num x, num y) {
-		_doCall("lineTo", [x, y]);
+		_doCall("lineTo", () { return [x / _unit, y / _unit]; });
 	}
 
 	void curveTo(num x1, num y1, num x2, num y2, num x3, num y3) {
-		_doCall("curveTo", [x1, y1, x2, y2, x3, y3]);
+		_doCall("curveTo", () {
+			return [
+				x1 / _unit, y1 / _unit,
+				x2 / _unit, y2 / _unit,
+				x3 / _unit, y3 / _unit
+			];
+		});
 	}
 
 	void circle(num x, num y, num r) {
-		_doCall("circle", [x, y, r]);
+		_doCall("circle", () {
+			return [x / _unit, y / _unit, r / _unit];
+		});
 	}
 
 	void arc(num x, num y, num r, num alpha, num beta, [ bool clockwise = false ]) {
-		_doCall("arc", [x, y, r, alpha, beta, clockwise]);
+		_doCall("arc", () {
+			return [x / _unit, y / _unit, r / _unit, alpha, beta, clockwise];
+		});
 	}
 
 	Future loadImage(String imagePath, [ String options = "" ]) {
@@ -200,7 +284,10 @@ class PDF {
 		f = Future.wait([image, f]).then((List responses) {
 			int imageHandle = responses[0];
 			ReceivePort response = new ReceivePort();
-			_servicePort.send([response.sendPort, "imageTo", handle, imageHandle, x, y, options]);
+			_servicePort.send([
+				response.sendPort, "imageTo", handle,
+				imageHandle, x / _unit, y / _unit, options
+			]);
 			return response.first;
 		}).then((List result) {
 			if (result[0] != null) {
@@ -219,8 +306,11 @@ class PDF {
 		_doCall("terminatePath", [what]);
 	}
 
-	void _doCall(String command, [ List args = null ]) {
+	void _doCall(String command, [ args = null ]) {
 		f = f.then((_) {
+			if (args is Function) {
+				args = (args as Function)();
+			}
 //			print("calling ${command}");
 			ReceivePort response = new ReceivePort();
 			List callArgs = [response.sendPort, command, handle ];
